f54084f6e715f9581dcd2a9421e085dd4a2f4ee2
brewing1 <brauny_13@hotmail.com>
2020-06-20 13:47:35 +0200

    Reads a pbn file and prints each deal in it

--- src/api.py
+++ src/api.py
@@ -2,8 +2,6 @@ from flask import Flask, request
 from flask_cors import CORS
 from flask_restful import Resource, Api
 
-from dds import DDS
-
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
@@ -14,14 +12,20 @@ class Convert(Resource):
     def get(self):
         return {'hello': 'world'}
 
-    def post(self):
+    def post(self, output):
         """Reads a file, verifies what type it is, and either converts it or rejects it"""
-        return
+        if 'file' not in request.files:
+            return {'status': 'no file'}
+        file_obj = request.files['file'] 
+        deals = file_obj.read().decode('utf-8').split('\n\n')
+        print(deals)
 
+        return {"status": "success"}
 
-api.add_resource(DDSTable, '/api/convert/')
+api.add_resource(Convert, '/api/convert/<output>')
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # curl -F "file=@/full/path/to/pbn" http://localhost:5000/api/convert/test
 
 
